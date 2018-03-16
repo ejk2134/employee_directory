eb23cea34497f6b42a30810611439f33aeaaaa7d
Evan Kearney <evanjkearney@gmail.com>
2018-03-15 23:21:25 -0500

    Prepare forms for edit after Get One request

--- app/employees/getOneEmployee.js
+++ app/employees/getOneEmployee.js
@@ -4,8 +4,22 @@ function getOneEmployee(id){
     $.ajax({
         type: 'GET',
         url: 'server/Employee/getOne.php?id=' + id,
-        success: (data)=>{
-            console.log(data);
+        success: (employeeData)=>{
+            console.log(employeeData);
+
+            // Fill current employee data in Edit Form
+            $('#last-name').val(employeeData.last_name)
+            $('#first-name').val(employeeData.first_name)
+            $('#title').val(employeeData.title)
+            $('#salary').val(employeeData.salary)
+            $('#address').val(employeeData.address)
+            $('#city').val(employeeData.city)
+            $('#state').val(employeeData.state)
+            $('#zip').val(employeeData.zip)
+            $('#phone').val(employeeData.phone)
+
+            // Attach employee ID to Submit Changes button
+            $('#edit-employee').data('employee-id', employeeData.id);        
         }
     })
 }
